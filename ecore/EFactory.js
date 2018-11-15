71a5d352a32820907361424c999a6d4d16bdd22f
Simon Schwichtenberg <schwicht@mail.upb.de>
2018-11-15 14:10:24 +0100

    Remove *.js and *.js.map from index

--- ecore/EFactory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* CrossEcore is a cross-platform modeling framework that generates C#, TypeScript,
- * JavaScript, Swift code from Ecore models with embedded OCL (http://www.crossecore.org/).
- * The original Eclipse Modeling Framework is available at https://www.eclipse.org/modeling/emf/.
- *
- * contributor: Simon Schwichtenberg
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=EFactory.js.map
